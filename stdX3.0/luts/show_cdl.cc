54781869c656a0f270dfb899d8bedac359e224db
sharktacos <derekvfx@gmail.com>
2021-08-14 13:58:34 -0700

    Add files via upload

---
+++ stdX3.0/luts/show_cdl.cc
@@ -0,0 +1,1 @@
+<ColorCorrection id=""><SOPNode><Description></Description><Slope>1 1 1</Slope><Offset>0 0 0</Offset><Power>0.97 1.09 0.85</Power></SOPNode><SatNode><Saturation>1</Saturation></SatNode></ColorCorrection>
